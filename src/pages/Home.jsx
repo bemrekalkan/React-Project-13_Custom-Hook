2142c15ea64e9e70941ac0f83f674e4406e0d0f1
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-22 19:18:26 +0300

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,71 +1,100 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 import axios from "axios";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 export default function SimpleContainer() {
-  
+  const [country, setCountry] = useState("");
+  const [year, setYear] = useState("");
+  const [holidays, setHolidays] = useState([]);
 
+  let url = 
 
   return (
     <Container maxWidth="md" sx={{ marginTop: "1rem" }}>
       <CssBaseline />
       <Box
-        sx={{ bgcolor: "#A1A2A6", height: "20vh", padding: "1rem", color:"#F0F0F2" }}
+        sx={{
+          bgcolor: "#A1A2A6",
+          height: "20vh",
+          padding: "1rem",
+          color: "#F0F0F2",
+        }}
         component="form"
       >
         <TextField
           placeholder="Please Enter Country..."
           variant="filled"
+          name="country"
+          value={country}
           type="search"
           InputProps={{ disableUnderline: true }}
+          onChange={(e) => {
+            setCountry(e.target.value);
+          }}
         />
         <TextField
           placeholder="Please Enter Year..."
           variant="filled"
           type="number"
+          value={year}
+          name={year}
           InputProps={{ disableUnderline: true }}
-          sx={{marginLeft:"20px", marginRight:"20px"}}
-          
+          sx={{ marginLeft: "20px", marginRight: "20px" }}
+          onChange={(e) => {
+            setYear(e.target.value);
+          }}
         />
-      <Button variant="standard" type="submit" sx={{border:"1px solid #3B3C40", marginTop:"10px", color:"#F0F0F2", "&:hover":{background:'#6E6F73'}}} >Search</Button>
+        <Button
+          variant="standard"
+          type="submit"
+          sx={{
+            border: "1px solid #3B3C40",
+            marginTop: "10px",
+            color: "#F0F0F2",
+            "&:hover": { background: "#6E6F73" },
+          }}
+        >
+          Search
+        </Button>
       </Box>
       <Box>
         <Typography variant="h3" component="h3" align="center">
-        {/* {year} */}
+          {/* {year} */}
         </Typography>
         <Typography variant="h4" component="h4" align="center">
           {/* Holidays for {country.toUpperCase()} */}
         </Typography>
-        <Typography sx={{textAlign:"center"}}>
-        {/* <img src={flag?.filter((c) => c.altSpellings[0] === country.toUpperCase())[0]?.flags.png} alt="" /> */}
+        <Typography sx={{ textAlign: "center" }}>
+          {/* <img src={flag?.filter((c) => c.altSpellings[0] === country.toUpperCase())[0]?.flags.png} alt="" /> */}
         </Typography>
         <Grid container>
-                <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650}} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell sx={{width:"150px"}}>Country Name</TableCell>
-            <TableCell align="left">Holiday Name</TableCell>
-            <TableCell align="left" sx={{width:"150px"}}>Date</TableCell>
-            <TableCell align="left">Description</TableCell>
-           
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {/* {holidays.map((item,index) => {
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell sx={{ width: "150px" }}>Country Name</TableCell>
+                  <TableCell align="left">Holiday Name</TableCell>
+                  <TableCell align="left" sx={{ width: "150px" }}>
+                    Date
+                  </TableCell>
+                  <TableCell align="left">Description</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {/* {holidays.map((item,index) => {
             const {country:{name:ctname}, date:{iso} , description : desc, name:hname, urlid } = item
             return(
             <TableRow
@@ -83,9 +112,9 @@ export default function SimpleContainer() {
               <TableCell align="left">{desc}</TableCell>
             </TableRow>
         )})} */}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Grid>
       </Box>
     </Container>
